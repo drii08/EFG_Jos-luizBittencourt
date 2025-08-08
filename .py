4138440aab831da4417378322bef601946c0d327
Adriane <165713446+drii08@users.noreply.github.com>
2025-08-08 16:16:58 -0300

    08/08/2025

---
+++ .py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos você tem?')
+cidade = input('Em qual cidade você mora?')
+
+print (f"Olá, {nome}! Você tem {idade} anos e é de {cidade}! Seja bem vindo(a)!" )
